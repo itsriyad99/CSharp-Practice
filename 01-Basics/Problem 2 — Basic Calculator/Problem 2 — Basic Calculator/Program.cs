9668ae719ab063948c055e114d77b983e8183aa7
= <=>
2026-09-25 11:09:08 +0600

    Add 2nd basic problem

---
+++ 01-Basics/Problem 2 — Basic Calculator/Problem 2 — Basic Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter 1st number: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Enter 2nd number: ");
+int b = int.Parse(Console.ReadLine());
+
+int addition = a + b;
+int subtraction = a - b;
+int multiplication = a * b;
+int division = a/b;
+int remainder = a % b;
+
+Console.WriteLine($"Addition: {addition}");
+Console.WriteLine($"Subtraction: {subtraction}");
+Console.WriteLine($"Multiplication: {multiplication}");
+Console.WriteLine($"Division: {division}");
+Console.WriteLine($"Remainder: {remainder}");
