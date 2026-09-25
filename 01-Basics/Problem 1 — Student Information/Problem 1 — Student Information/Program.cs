292a34928935b9415e7f7bfe4b286b6528bcc688
= <=>
2026-09-25 11:04:46 +0600

    add problem-1

---
+++ 01-Basics/Problem 1 — Student Information/Problem 1 — Student Information/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Write("Enter the student's name:");
+string name = Console.ReadLine();
+Console.Write("Enter the student's age:");
+int age = int.Parse(Console.ReadLine());
+Console.Write("Enter the student's grade:");
+string grade = Console.ReadLine();
+
+Console.WriteLine("Student Information");
+Console.WriteLine("-------------------");
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Grade: {grade}");
